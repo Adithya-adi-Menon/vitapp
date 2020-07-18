4a52e390d1cebcaf050f52c344e5cbeb5192d81a
cipherinfoline <cipherinfoline@gmail.com>
2020-07-18 12:46:30 +0530

    added reg backend-adi

--- app/src/main/java/com/example/vitapp/MainActivity.java
+++ app/src/main/java/com/example/vitapp/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.vitapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText name,regno,phone;
+    Button register;
+    user_db user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,20 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        name=(EditText)findViewById(R.id.name);
+        regno=(EditText)findViewById(R.id.regno);
+        phone=(EditText)findViewById(R.id.mobileno);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert();
+            }
+        });
+
+
+    }
+    private void insert()
+    {
 
     }
 }
---
+++ app/src/main/java/com/example/vitapp/user_db.java
@@ -0,0 +1,34 @@
+package com.example.vitapp;
+public class user_db {
+
+    private  String name;
+    private Long phone;
+    private String regno;
+
+    public user_db() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getRegno() {
+        return regno;
+    }
+
+    public void setRegno(String regno) {
+        this.regno = regno;
+    }
+}
