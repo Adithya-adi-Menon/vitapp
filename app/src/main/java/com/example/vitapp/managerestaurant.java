24c390beb913fd42d8e7d3104a49ff0539ad9341
cipherinfoline <cipherinfoline@gmail.com>
2020-07-19 21:42:44 +0530

    new commint-adi

---
+++ app/src/main/java/com/example/vitapp/managerestaurant.java
@@ -0,0 +1,14 @@
+package com.example.vitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class managerestaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_managerestaurant);
+    }
+}
