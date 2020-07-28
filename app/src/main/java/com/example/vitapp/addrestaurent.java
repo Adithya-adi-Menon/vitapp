b02fe42f95db495d5eb137d49027e47657a7d374
Lebron Yogi <bkyogesh28@gmail.com>
2020-07-28 12:38:29 +0530

    ADmin updated

--- app/src/main/java/com/example/vitapp/addrestaurent.java
+++ app/src/main/java/com/example/vitapp/addrestaurent.java
@@ -9,23 +9,36 @@ import com.example.vitapp.ui.notifications.profileFragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class addrestaurent extends AppCompatActivity {
     Button add;
     TextInputEditText name;
     String nameres;
+    user_db user1;
+    DatabaseReference refer1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_managerestaurant);
         add=findViewById(R.id.add);
         name=(TextInputEditText) findViewById(R.id.name);
+        FirebaseApp.initializeApp(this);
+        refer1= FirebaseDatabase.getInstance().getReference().child("AdminQR");
+        user1=new user_db();
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 nameres=name.getText().toString();
               if(nameres.isEmpty()==true){
                   Toast.makeText(addrestaurent.this,"Name cannot be empty",Toast.LENGTH_LONG).show();
+              }else {
+
+               user1.setName(nameres);
+               refer1.push().setValue(user1);
+
               }
 
             }
--- app/src/main/java/com/example/vitapp/managerestaurant.java
+++ app/src/main/java/com/example/vitapp/managerestaurant.java
@@ -3,6 +3,11 @@ package com.example.vitapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class managerestaurant extends AppCompatActivity {
 
@@ -10,5 +15,7 @@ public class managerestaurant extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_managerestaurant);
+
+
     }
 }
